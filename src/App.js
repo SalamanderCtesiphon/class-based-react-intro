06e42aff08a7de644eed1c41cba2c3c2fe8009cf
salamanderctesiphon <swbrookshire@gmail.com>
2023-04-03 10:55:58 -0500

    added a component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import './App.css';
+import React, { Component } from 'react';
+import MyComponent from './components/MyComponent';
 
-function App() {
-  return (
-    <div className="App">
-     
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <MyComponent title = "React" />
+      </div>
+    );
+  }
+ 
 }
 
 export default App;
