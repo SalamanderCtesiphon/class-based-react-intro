b181935054874c06abe4f7abccf64f1fe8def7d2
salamanderctesiphon <swbrookshire@gmail.com>
2023-04-03 11:04:26 -0500

    following along with the lesson

--- src/App.js
+++ src/App.js
@@ -5,12 +5,18 @@ import MyComponent from './components/MyComponent';
 class App extends Component {
   constructor(props) {
     super(props);
+
+    this.onClickBtn = this.onClickBtn.bind(this);
+  }
+
+  onClickBtn() {
+    console.log("Button has been clicked!");
   }
 
   render() {
     return (
       <div className="App">
-        <MyComponent title = "React" />
+        <MyComponent title = "React" onButtonClicked={this.onClickBtn}/>
       </div>
     );
   }
